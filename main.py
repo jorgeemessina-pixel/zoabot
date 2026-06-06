a4e5bb70345022e7faff2ae9334954855be10045
jorgeemessina-pixel <jorgeemessina@gmail.com>
2026-06-06 20:49:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hola Jorge! Zoabot está vivo 🐯")
 
-app = Application.builder().token(os.getenv("TOKEN")).build()
+app = Application.builder().token(os.getenv("TELEGRAM_BOT_TOKEN")).build()
 app.add_handler(CommandHandler("start", start))
 
 if __name__ == "__main__":

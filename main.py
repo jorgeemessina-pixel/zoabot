f4835b12e3ae7456e9c2c6c41e92765d18ff6566
jorgeemessina-pixel <jorgeemessina@gmail.com>
2026-06-06 22:54:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 import os
 import asyncio
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hola! Zoabot está vivo 🐯")
-
+    await update.message.reply_text("Hola! Zoabot esta vivo!")
 async def main():
     app = Application.builder().token(os.getenv("TELEGRAM_BOT_TOKEN")).build()
     app.add_handler(CommandHandler("start", start))
     await app.run_polling()
-
 if __name__ == "__main__":
+    asyncio.run(main())
